9724ee5c0249996b40af90b38a90ca0fb88023b0
andrew <andrew.kostyukevich@gmail.com>
2021-09-15 17:09:10 +0300

    solve the equation

---
+++ hm1.js
@@ -0,0 +1,26 @@
+let example = [1,0,-4];
+
+const solveEquation = (arr) => {
+    let [a,b,c] = arr;
+
+    let equation = `${a} * x^2 + ${b} * x + ${c} = 0`;
+
+    let discriminant = Math.pow(b,2) - (4 * a * c);
+
+    if(discriminant < 0) {
+        return console.log(`No Roots!`);
+    } else if(discriminant === 0) {
+        return `x: ${(-b + Math.sqrt(discriminant)) / 2 * a}`;
+    } else {
+        return console.log(
+            `
+            Equation: ${equation},
+            Discriminant: ${discriminant},
+            Roots: 
+                x1: ${(-b + Math.sqrt(discriminant)) / 2 * a}, 
+                x2: ${(-b - Math.sqrt(discriminant)) / 2 * a}
+            `);
+    }
+}
+
+solveEquation(example);
