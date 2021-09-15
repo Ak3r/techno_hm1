7af602a7033336cda02214bde0bf9750b2ec7b9d
andrew <andrew.kostyukevich@gmail.com>
2021-09-15 19:32:09 +0300

    add switch

--- hm1.js
+++ hm1.js
@@ -7,31 +7,35 @@ const solveEquation = (arr) => {
 
     let discriminant = Math.pow(b,2) - (4 * a * c);
 
-    if(discriminant < 0) {
-        return console.log(
-            `
-            Equation: ${equation},
-            Discriminant: ${discriminant},
-            Roots: 
-                No Roots!
-            `);
-    } else if(discriminant === 0) {
-        return console.log(
-            `
-            Equation: ${equation},
-            Discriminant: ${discriminant},
-            Roots: 
-                x: ${(-b + Math.sqrt(discriminant)) / 2 * a}
-            `);
-    } else {
-        return console.log(
-            `
-            Equation: ${equation},
-            Discriminant: ${discriminant},
-            Roots: 
-                x1: ${(-b + Math.sqrt(discriminant)) / 2 * a}, 
-                x2: ${(-b - Math.sqrt(discriminant)) / 2 * a}
-            `);
+    switch(discriminant > 0) {
+        case true: 
+            console.log(
+                `
+                Equation: ${equation},
+                Discriminant: ${discriminant},
+                Roots: 
+                    x1: ${(-b + Math.sqrt(discriminant)) / 2 * a}, 
+                    x2: ${(-b - Math.sqrt(discriminant)) / 2 * a}
+                `);
+            break;
+        case false: 
+            console.log(
+                `
+                Equation: ${equation},
+                Discriminant: ${discriminant},
+                Roots: 
+                    No Roots!
+                `);
+            break;
+        default:
+            console.log(
+                `
+                Equation: ${equation},
+                Discriminant: ${discriminant},
+                Roots: 
+                    x: ${(-b + Math.sqrt(discriminant)) / 2 * a}
+                `);
+            break;
     }
 }
 
